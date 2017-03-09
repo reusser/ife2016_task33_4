034aa479709181dd469afbcce484edd0f4c4f086
oyh <919917215@qq.com>
2017-03-09 16:54:20 +0800

    update version to 2.0

--- task33.js
+++ task33.js
@@ -10,18 +10,18 @@ let carController = {
 	location: [5, 5],
 	deg: 0,
 	init() {
-                this.setCarSize();
+    this.setCarSize();
 	},
 	//设置定位 x为行数最小为1 y为列数最小为1
 	setCarSize() {
 		let ele = document.getElementsByTagName('ul')[1].getElementsByTagName('li')[1];
-                this.car.style.height = window.getComputedStyle(ele, null).getPropertyValue('height');    		
-	        this.car.style.width  = window.getComputedStyle(ele, null).getPropertyValue('width');
+    this.car.style.height = window.getComputedStyle(ele, null).getPropertyValue('height');    		
+	  this.car.style.width  = window.getComputedStyle(ele, null).getPropertyValue('width');
 	},
 	setCarLocation([x, y]) {
 		if (x <= 0 || x >= 11 || y <= 0 || y >= 11) return alert('超出了活动范围！');
-	        this.car.style.transform = `translateX(${100 * y}%) translateY(${100 * x}%) rotateZ(${this.deg}deg)`;
-	        this.location = [x, y];
+    this.car.style.transform = `translateX(${100 * y}%) translateY(${100 * x}%) rotateZ(${this.deg}deg)`;
+    this.location = [x, y];
 	},
 	//向左转
 	turnLeft() {
@@ -39,10 +39,12 @@ let carController = {
 		this.deg = this.deg + 90;
 		this.rotateCar();
 	},
+	//向后转
 	turnBack() {
 		this.turnRight();
 		setTimeout(() => this.turnRight(), 250);
 	},
+	//前进
 	go() {
 		if (this.directionNow === 'up') {
 
@@ -62,9 +64,55 @@ let carController = {
 			
 		}
 	},
+	//向左移动一格，方向不变
+	traLeft() {
+		this.setCarLocation([this.location[0], this.location[1] - 1]);
+	},
+	//向右移动一格，方向不变
+	traRight() {
+		this.setCarLocation([this.location[0], this.location[1] + 1]);
+	},
+	//向上移动一格，方向不变
+	traTop() {
+		this.setCarLocation([this.location[0] - 1, this.location[1]]);
+	},
+	//向下移动一格，方向不变
+	traBottom() {
+    this.setCarLocation([this.location[0] + 1, this.location[1]]);
+	},
+	//方向转向屏幕上面，向屏幕的上面移动一格
+	movTop() {
+    this.directionPointer >= 2 ? this.deg += 90 * this.directionPointer : this.deg -= 90 * this.directionPointer;
+    this.directionPointer = 0;
+    this.directionNow = this.directionBox[this.directionPointer];
+    this.rotateCar();
+    setTimeout( () => this.go(), 500);
+	},
+	//方向转向屏幕右侧，向屏幕的右侧移动一格
+	movRight() {
+    (this.directionPointer === 0 || this.directionPointer === 3) ? this.deg += 90 * Math.abs(this.directionPointer - 1) : this.deg -= 90 * (this.directionPointer - 1);
+    this.directionPointer = 1;
+    this.directionNow = this.directionBox[this.directionPointer]; 
+    this.rotateCar();
+    setTimeout( () => this.go(), 500);
+	},
+	movBottom() {
+		this.directionPointer <= 1 ? this.deg += 90 * Math.abs(this.directionPointer - 2) : this.deg -= 90 * (this.directionPointer - 2);
+		this.directionPointer = 2;
+		this.directionNow = this.directionBox[this.directionPointer];
+		this.rotateCar();
+		setTimeout( () => this.go(), 500);
+	},
+	movLeft() {
+		this.directionPointer === 0 ? this.deg -= 90 : this.deg += 90 * Math.abs(this.directionPointer - 3);
+		this.directionPointer = 3;
+		this.directionNow = this.directionBox[this.directionPointer];
+		this.rotateCar();
+		setTimeout( () => this.go(), 500);
+	},
 	rotateCar() {
-                let [x, y] = this.location;
-                this.car.style.transform = `translateX(${100 * y}%) translateY(${100 * x}%) rotateZ(${this.deg}deg)`;
+    let [x, y] = this.location;
+    this.car.style.transform = `translateX(${100 * y}%) translateY(${100 * x}%) rotateZ(${this.deg}deg)`;
 	}
 };
 
@@ -75,6 +123,14 @@ let cmdHandler = (text) => {
 	else if (text === 'tunlef') carController.turnLeft();
 	else if (text === 'tunrig') carController.turnRight();
 	else if (text === 'tunbac') carController.turnBack();
+	else if (text === 'tralef') carController.traLeft();
+	else if (text === 'trarig') carController.traRight();
+	else if (text === 'trabot') carController.traBottom();
+	else if (text === 'tratop') carController.traTop();
+	else if (text === 'movtop') carController.movTop();
+	else if (text === 'movrig') carController.movRight();
+	else if (text === 'movbot') carController.movBottom();
+	else if (text === 'movlef') carController.movLeft()
 	else alert('不存在此命令');
 }
 
