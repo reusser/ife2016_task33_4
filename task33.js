47de971d96626e8cba40cc73a3ec617d55a9db1d
oyh <919917215@qq.com>
2017-03-11 16:41:04 +0800

    update version to 3.0

--- task33.js
+++ task33.js
@@ -3,140 +3,140 @@
  * @type {Object}
  */
 let carController = {
-	car: document.getElementById('car'),
-	directionBox: ['up', 'right', 'down', 'left'],
-	directionPointer: 0,
-	directionNow: 'up',
-	location: [5, 5],
-	deg: 0,
-	init() {
+  car: document.getElementById('car'),
+  directionBox: ['up', 'right', 'down', 'left'],
+  directionPointer: 0,
+  directionNow: 'up',
+  location: [5, 5],
+  deg: 0,
+  init() {
     this.setCarSize();
-	},
-	//设置定位 x为行数最小为1 y为列数最小为1
-	setCarSize() {
+  },
+  //设置定位 x为行数最小为1 y为列数最小为1
+  setCarSize() {
     let ele = document.getElementsByTagName('ul')[1].getElementsByTagName('li')[1];
-    this.car.style.height = window.getComputedStyle(ele, null).getPropertyValue('height');    		
+    this.car.style.height = window.getComputedStyle(ele, null).getPropertyValue('height');        
     this.car.style.width  = window.getComputedStyle(ele, null).getPropertyValue('width');
-	},
-	setCarLocation([x, y]) {
+  },
+  setCarLocation([x, y]) {
     if (x <= 0 || x >= 11 || y <= 0 || y >= 11) return alert('超出了活动范围！');
     this.car.style.transform = `translateX(${100 * y}%) translateY(${100 * x}%) rotateZ(${this.deg}deg)`;
     this.location = [x, y];
-	},
-	//向左转
-	turnLeft() {
-		if (this.directionPointer === 0) this.directionPointer = 3;
-		else this.directionPointer -= 1;
-		this.directionNow = this.directionBox[this.directionPointer];
-		this.deg = this.deg - 90;
-		this.rotateCar();
-	},
-	//向右转
-	turnRight() {
-		if (this.directionPointer === 3) this.directionPointer = 0;
-		else this.directionPointer += 1;
-		this.directionNow = this.directionBox[this.directionPointer];
-		this.deg = this.deg + 90;
-		this.rotateCar();
-	},
-	//向后转
-	turnBack() {
-		this.turnRight();
-		setTimeout(() => this.turnRight(), 250);
-	},
-	//前进
-	go(moveNum) {
-		if (this.directionNow === 'up') {
+  },
+  //向左转
+  turnLeft() {
+    if (this.directionPointer === 0) this.directionPointer = 3;
+    else this.directionPointer -= 1;
+    this.directionNow = this.directionBox[this.directionPointer];
+    this.deg = this.deg - 90;
+    this.rotateCar();
+  },
+  //向右转
+  turnRight() {
+    if (this.directionPointer === 3) this.directionPointer = 0;
+    else this.directionPointer += 1;
+    this.directionNow = this.directionBox[this.directionPointer];
+    this.deg = this.deg + 90;
+    this.rotateCar();
+  },
+  //向后转
+  turnBack() {
+    this.turnRight();
+    setTimeout(() => this.turnRight(), 250);
+  },
+  //前进
+  go(moveNum) {
+    if (this.directionNow === 'up') {
 
-			this.setCarLocation([this.location[0] - moveNum, this.location[1]]);
+      this.setCarLocation([this.location[0] - moveNum, this.location[1]]);
 
-		} else if (this.directionNow === 'right') {
+    } else if (this.directionNow === 'right') {
 
-			this.setCarLocation([this.location[0], this.location[1] + moveNum]);
+      this.setCarLocation([this.location[0], this.location[1] + moveNum]);
 
-		} else if (this.directionNow === 'down') {
+    } else if (this.directionNow === 'down') {
 
-			this.setCarLocation([this.location[0] + moveNum, this.location[1]]);
+      this.setCarLocation([this.location[0] + moveNum, this.location[1]]);
 
-		} else {
+    } else {
 
-			this.setCarLocation([this.location[0], this.location[1] - moveNum]);
-			
-		}
-	},
-	//向左移动一格，方向不变
-	traLeft(moveNum) {
-		this.setCarLocation([this.location[0], this.location[1] - moveNum]);
-	},
-	//向右移动一格，方向不变
-	traRight(moveNum) {
-		this.setCarLocation([this.location[0], this.location[1] + moveNum]);
-	},
-	//向上移动一格，方向不变
-	traTop(moveNum) {
-		this.setCarLocation([this.location[0] - moveNum, this.location[1]]);
-	},
-	//向下移动一格，方向不变
-	traBottom(moveNum) {
+      this.setCarLocation([this.location[0], this.location[1] - moveNum]);
+      
+    }
+  },
+  //向左移动一格，方向不变
+  traLeft(moveNum) {
+    this.setCarLocation([this.location[0], this.location[1] - moveNum]);
+  },
+  //向右移动一格，方向不变
+  traRight(moveNum) {
+    this.setCarLocation([this.location[0], this.location[1] + moveNum]);
+  },
+  //向上移动一格，方向不变
+  traTop(moveNum) {
+    this.setCarLocation([this.location[0] - moveNum, this.location[1]]);
+  },
+  //向下移动一格，方向不变
+  traBottom(moveNum) {
     this.setCarLocation([this.location[0] + moveNum, this.location[1]]);
-	},
-	//方向转向屏幕上面，向屏幕的上面移动一格
-	movTop(moveNum) {
+  },
+  //方向转向屏幕上面，向屏幕的上面移动一格
+  movTop(moveNum) {
     this.directionPointer >= 2 ? this.deg += 90 * this.directionPointer : this.deg -= 90 * this.directionPointer;
     this.directionPointer = 0;
     this.directionNow = this.directionBox[this.directionPointer];
     this.rotateCar();
     setTimeout( () => this.go(moveNum), 500);
-	},
-	//方向转向屏幕右侧，向屏幕的右侧移动一格
-	movRight(moveNum) {
+  },
+  //方向转向屏幕右侧，向屏幕的右侧移动一格
+  movRight(moveNum) {
     (this.directionPointer === 0 || this.directionPointer === 3) ? this.deg += 90 * Math.abs(this.directionPointer - 1) : this.deg -= 90 * (this.directionPointer - 1);
     this.directionPointer = 1;
     this.directionNow = this.directionBox[this.directionPointer]; 
     this.rotateCar();
     setTimeout( () => this.go(moveNum), 500);
-	},
-	movBottom(moveNum) {
-		this.directionPointer <= 1 ? this.deg += 90 * Math.abs(this.directionPointer - 2) : this.deg -= 90 * (this.directionPointer - 2);
-		this.directionPointer = 2;
-		this.directionNow = this.directionBox[this.directionPointer];
-		this.rotateCar();
-		setTimeout( () => this.go(moveNum), 500);
-	},
-	movLeft(moveNum) {
-		this.directionPointer === 0 ? this.deg -= 90 : this.deg += 90 * Math.abs(this.directionPointer - 3);
-		this.directionPointer = 3;
-		this.directionNow = this.directionBox[this.directionPointer];
-		this.rotateCar();
-		setTimeout( () => this.go(moveNum), 500);
-	},
-	rotateCar() {
+  },
+  movBottom(moveNum) {
+    this.directionPointer <= 1 ? this.deg += 90 * Math.abs(this.directionPointer - 2) : this.deg -= 90 * (this.directionPointer - 2);
+    this.directionPointer = 2;
+    this.directionNow = this.directionBox[this.directionPointer];
+    this.rotateCar();
+    setTimeout( () => this.go(moveNum), 500);
+  },
+  movLeft(moveNum) {
+    this.directionPointer === 0 ? this.deg -= 90 : this.deg += 90 * Math.abs(this.directionPointer - 3);
+    this.directionPointer = 3;
+    this.directionNow = this.directionBox[this.directionPointer];
+    this.rotateCar();
+    setTimeout( () => this.go(moveNum), 500);
+  },
+  rotateCar() {
     let [x, y] = this.location;
     this.car.style.transform = `translateX(${100 * y}%) translateY(${100 * x}%) rotateZ(${this.deg}deg)`;
-	}
+  }
 };
 
 /************************************** 命令部分 **********************************/
 let cmdHandler = (text, index) => {
-	text         = text.toLowerCase();
-	text         = text.replace(/\s/g, '');
-	let moveNum  = 1;
-	let numIndex = text.search(/\d+/);
-	if (numIndex >= 2) moveNum = parseInt(text.substring(numIndex));
-	text = text.replace(/\d/g, '');
-	if (text === 'go') carController.go(moveNum);
-	else if (text === 'tunlef') carController.turnLeft();
-	else if (text === 'tunrig') carController.turnRight();
-	else if (text === 'tunbac') carController.turnBack();
-	else if (text === 'tralef') carController.traLeft(moveNum);
-	else if (text === 'trarig') carController.traRight(moveNum);
-	else if (text === 'trabot') carController.traBottom(moveNum);
-	else if (text === 'tratop') carController.traTop(moveNum);
-	else if (text === 'movtop') carController.movTop(moveNum);
-	else if (text === 'movrig') carController.movRight(moveNum);
-	else if (text === 'movbot') carController.movBottom(moveNum);
-	else if (text === 'movlef') carController.movLeft(moveNum)
-	else errorInfo(index);
+  text         = text.toLowerCase();
+  text         = text.replace(/\s/g, '');
+  let moveNum  = 1;
+  let numIndex = text.search(/\d+/);
+  if (numIndex >= 2) moveNum = parseInt(text.substring(numIndex));
+  text = text.replace(/\d/g, '');
+  if (text === 'go') carController.go(moveNum);
+  else if (text === 'tunlef') carController.turnLeft();
+  else if (text === 'tunrig') carController.turnRight();
+  else if (text === 'tunbac') carController.turnBack();
+  else if (text === 'tralef') carController.traLeft(moveNum);
+  else if (text === 'trarig') carController.traRight(moveNum);
+  else if (text === 'trabot') carController.traBottom(moveNum);
+  else if (text === 'tratop') carController.traTop(moveNum);
+  else if (text === 'movtop') carController.movTop(moveNum);
+  else if (text === 'movrig') carController.movRight(moveNum);
+  else if (text === 'movbot') carController.movBottom(moveNum);
+  else if (text === 'movlef') carController.movLeft(moveNum)
+  else errorInfo(index);
 }
 
 let cmdBtn       = document.getElementById('cmd-btn');
@@ -150,31 +150,31 @@ let rowNowLength = rowNum.length;
 
 //错误提示
 const errorInfo = (index) => {
-	let target = index > 0 ? rowOl.getElementsByTagName('div')[index - 1] : topRow;
-	target.style.backgroundColor = '#d93a49';
+  let target = index > 0 ? rowOl.getElementsByTagName('div')[index - 1] : topRow;
+  target.style.backgroundColor = '#d93a49';
 }
 
 cmdBtn.onclick = () => {
-	resetRow();
-	let textArray = cmdArea.value.split(/\n/);
-	let i = 0;
-	let animate   = setInterval( () => {
+  resetRow();
+  let textArray = cmdArea.value.split(/\n/);
+  let i = 0;
+  let animate   = setInterval( () => {
     if (i >= textArray.length) {
-    	clearInterval(animate);
-    	return;
+      clearInterval(animate);
+      return;
     }
     
     let text = textArray[i];
-  	text = text.trim();
-  	text === '' ? errorInfo(i) : cmdHandler(text, i); 
-  	i++;	
-	}, 500);
+    text = text.trim();
+    text === '' ? errorInfo(i) : cmdHandler(text, i); 
+    i++;  
+  }, 500);
 };
 
 const resetRow = () => {
   topRow.style.backgroundColor = '#ccc';
   Array.from(rowOl.getElementsByTagName('div')).forEach( (item) => {
-  	item.style.backgroundColor = '#ccc';
+    item.style.backgroundColor = '#ccc';
   } )
 };
 
@@ -186,31 +186,31 @@ reBtn.onclick = () => {
 };
 /************************************** 文本部分 **********************************/
 cmdArea.onkeydown = (event) => {
-	if (event.keyCode == 13) {
+  if (event.keyCode == 13) {
 
-		rowNum.push(rowNum.length + 1);
-		rowNowLength = rowNum.length;
-		renderRowNum();
-		topRow.style.backgroundColor = '#ccc';
+    rowNum.push(rowNum.length + 1);
+    rowNowLength = rowNum.length;
+    renderRowNum();
+    topRow.style.backgroundColor = '#ccc';
 
-	} else if (event.keyCode == 8) {
-		setTimeout( () => {
+  } else if (event.keyCode == 8) {
+    setTimeout( () => {
 
-		  rowNum.length = cmdArea.value.match(/\n/g) ? cmdArea.value.match(/\n/g).length + 1 : 1;
-		  if (rowNum.length !== rowNowLength) {
-			  rowNowLength = rowNum.length;
-			  renderRowNum();
-		  }	
-        topRow.style.backgroundColor = '#ccc';
+      rowNum.length = cmdArea.value.match(/\n/g) ? cmdArea.value.match(/\n/g).length + 1 : 1;
+      if (rowNum.length !== rowNowLength) {
+        rowNowLength = rowNum.length;
+        renderRowNum();
+      } 
+      topRow.style.backgroundColor = '#ccc';
 
-		}, 0);
-	}
+    }, 0);
+  }
 };
 
 const renderRowNum = () => {
   let rows = '';
   for (let i = 1; i < rowNum.length; i++) {
-  	rows += `<div>${rowNum[i]}</div>`;
+    rows += `<div>${rowNum[i]}</div>`;
   }
   rowOl.innerHTML = rows;
 }
